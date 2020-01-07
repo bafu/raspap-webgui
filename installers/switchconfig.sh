55ccf931c63e4fbf72d374c6cf193679e6d6d554
Bofu Chen (bafu) <bafuchen@gmail.com>
2020-01-08 00:19:29 +0800

    Update switchconfig.sh for Dyda.
    Signed-off-by: Bofu Chen (bafu) <bafuchen@gmail.com>

--- installers/switchconfig.sh
+++ installers/switchconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sensitivity=$1
 echo "Pipeline config sensitivity: $sensitivity"
-config_source=/etc/trainer/pipelines/configs/aikea/object_detection_and_tracking_aikea_${sensitivity}.config 
-config_target=/etc/trainer/pipelines/configs/object_detection_and_tracking_aikea.config
+config_source=/etc/dyda/pipelines/configs/aikea/object_detection_and_tracking_aikea_${sensitivity}.config
+config_target=/etc/dyda/pipelines/configs/object_detection_and_tracking_aikea.config
 unlink $config_target
 ln -s $config_source $config_target
